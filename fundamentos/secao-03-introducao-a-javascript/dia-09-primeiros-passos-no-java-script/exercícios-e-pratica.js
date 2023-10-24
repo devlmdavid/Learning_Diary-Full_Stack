def746ad4884a51efe42fd44b0b673cd84b85a40
devlmdavid <david.lm.augusto@gmail.com>
2023-10-23 23:02:37 -0300

    feito até o exercício 5 do dia 09

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-09-primeiros-passos-no-java-script/exercícios-e-pratica.js
@@ -0,0 +1,75 @@
+// Exercício 1
+
+// let num1 =5;
+// let num2 =5;
+// let num3 =6;
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+
+// Exercício 2
+
+// if (num1 > num2) {
+//     console.log('Sim, num1 é maior!')
+// } else {
+//     console.log('Num2 é maior!');
+// }
+
+// Exercício 3
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log('Num1 é maior!')
+// } else if (num2 > num1 && num2 > num3) {
+//     console.log('Num2 é maior!');
+// } else if (num3 > num1 && num3 > num2) {
+//     console.log('Num3 é maior!');
+// }
+
+
+// Exercício 4
+
+// let anguloA = 70;
+// let anguloB = 20;
+// let anguloC = 90;
+
+// let somaABC = anguloA + anguloB + anguloC;
+
+// let todosAngulos = anguloA > 0 && anguloB > 0 && anguloC > 0;
+
+// if (todosAngulos){
+//     if (somaABC === 180) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     };    
+// } else {
+//     console.log('Erro: ângulo inválido')
+// }
+
+
+// Exercício 5
+
+// let xadrez = "Peão";
+
+// switch (xadrez) {
+//     case 'Bispo':
+//         console.log('Pode andar na diagonal!')
+//         break;
+//     case 'Torre':
+//         console.log('Pode andar reto!')
+//         break;
+//     case 'Cavalo':
+//         console.log('Pode andar em L!')
+//         break;
+//     default: 
+//         console.log('Não é uma peça de xadrez')
+//         break;
+// }
+
+
+// Exercício 6
+
