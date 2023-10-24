00924b913ecabc7964a3526e45750bfa77087289
devlmdavid <david.lm.augusto@gmail.com>
2023-10-24 17:32:48 -0300

    terminado os exercício do dia 09

--- fundamentos/secao-03-introducao-a-javascript/dia-09-primeiros-passos-no-java-script/exercícios-e-pratica.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-09-primeiros-passos-no-java-script/exercícios-e-pratica.js
@@ -73,3 +73,49 @@
 
 // Exercício 6
 
+// let num1 = 4;
+// let num2 = 5;
+// let num3 = 3;
+
+// if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
+
+
+// Exercício 7
+
+let salario = 3000
+let inss;
+let ir;
+
+if (salario <= 1556.94) {
+    inss = salario * 0.08;
+} else if (salario > 1556.95 && salario <=2594.92) {
+    inss = salario * 0.09;
+} else if (salario > 2594.92 && salario <= 5189.82) {
+    inss = salario * 0.11;
+} else {
+    console.log(inss = 570.88);
+}
+console.log(inss)
+
+salario = salario - inss;
+
+if (salario <= 1903.98) {
+    ir = 0;
+} else if (salario >= 1903.99 && salario <= 2826.65) {
+    ir = salario * 0.075 - 142,80;
+} else if (salario >= 2826.66 && salario <= 3751.05) {
+    ir = salario * 0.15 - 354.8;
+} else if (salario >= 3751.06 && salario <=4664.68){
+    ir = salario * 0.225 - 636.13;
+} else {
+    ir =salario * 0.275 - 869.36;
+}
+console.log(ir)
+
+salario = salario - ir;
+
+console.log(salario);
