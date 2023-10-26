21b1fc07fcecac8370acab189babee2d478a72ec
devlmdavid <david.lm.augusto@gmail.com>
2023-10-26 15:14:34 -0300

    alguns exercícios de funções

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-11-funcoes/exercicio-funcoes.js
@@ -0,0 +1,28 @@
+// Exercício 01
+
+// const balance = 100;
+
+// function sumBalance(value) {
+//   return balance + value;
+// }
+
+// function subBalance(value) {
+//   return balance - value;
+// }
+
+// function multBalance(value) {
+//   return balance * value;
+// }
+
+// function divBalance(value) {
+//   return balance / value;
+// }
+
+// console.log(sumBalance)
+// console.log(subBalance)
+// console.log(multBalance)
+// console.log(divBalance)
+
+
+// Exercício 02
+
