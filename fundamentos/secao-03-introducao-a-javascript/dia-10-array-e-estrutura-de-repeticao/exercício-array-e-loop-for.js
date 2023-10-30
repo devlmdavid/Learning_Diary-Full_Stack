311d80efba1d4a01e663d85c5851325382fc7d3c
devlmdavid <david.lm.augusto@gmail.com>
2023-10-30 18:14:43 -0300

    exercícios dia 10

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-10-array-e-estrutura-de-repeticao/exercício-array-e-loop-for.js
@@ -0,0 +1,25 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    // qual a soma dos elementos e qual a média do valor total
+//     soma = soma + numbers[index];
+// }
+// let media = soma /2;
+// if (media > 20) {
+//     console.log(`O valor da média aritmética é maior que 20`);
+// } else if (media <= 20){
+//     console.log(`O valor da média aritmética é menor ou igual a 20`)
+// }
+// console.log(media);
+
+//  Qual o maior número
+//     if (soma < numbers[index]) {
+//     soma = numbers[index];
+//     };
+// };
+// console.log(soma);
+if (numbers[index] % 2 !== 0) {
+    let impar =
+}
+console.log(`${numbers} é impar`)
+};
