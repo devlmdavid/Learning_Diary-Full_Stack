a16d751fedeb67cb8c07386aa835238ea67b1578
devlmdavid <david.lm.augusto@gmail.com>
2023-11-13 23:43:25 -0300

    Adicionando exercício travel forms

---
+++ fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-21-html-e-css-forms/entenda-o-prevent-default.js
@@ -0,0 +1,4 @@
+// SELETORES
+const inputText = document.querySelector("#input-text");
+const inputCheckbox = document.querySelector("#input-checkbox");
+const hrefLink = document.querySelector("#href");
